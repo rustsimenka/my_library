c5690064904416637ba8e23b70fb2d8b23e42dd4
Руслан Устименко <rustsimenka@gmail.com>
2025-01-08 11:44:19 +0300

    6.1 сделать так, чтобы название книги состояло хотя бы из 4 символов

---
+++ backend/library/serialiser.py
@@ -0,0 +1,17 @@
+from django.core.validators import MinLengthValidator
+from rest_framework import serializers
+from .models import Book, Author
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'pages', 'author']
+
+    title = serializers.CharField(validators=[MinLengthValidator(4)])
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['id', 'first_name']

8e2f97a5557a0df82335d65240df87a77e89ee78
Руслан Устименко <rustsimenka@gmail.com>
2025-01-09 18:36:00 +0300

    7. Сделать отдельный эндпоинт, который по Книга.id вернет {"namename": "Книга.nameКнига.name"}

--- backend/library/viewsets.py
+++ backend/library/viewsets.py
@@ -1,3 +1,6 @@
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from library.models import Book, Author
@@ -7,6 +10,14 @@ from library.serialiser import BookSerializer, AuthorSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    @action(detail=True, methods=['get'])
+    def get_name_book(self, request_id, pk=None):
+        try:
+            book = self.get_object()
+            return Response({f'Под id {pk} записана книга: {book.title}'})
+        except Book.DoesNotExist:
+            return Response({'Книга не найдена'})
 
 
 class AuthorViewSet(ModelViewSet):

8ffd7f375c0f3e45b0bc6f9bb436ca810a155d1d
Руслан Устименко <rustsimenka@gmail.com>
2025-01-14 13:04:45 +0300

    fix: fix code

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -32,13 +32,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
 from rest_framework.routers import DefaultRouter
-from library.viewsets import BookViewSet, AuthorViewSet
 from rest_framework import permissions
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from library.viewsets import BookViewSet, AuthorViewSet
 
 ...
 
